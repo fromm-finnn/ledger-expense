2eaa0df4bcbc60526b20ef47262a9762200b5033
fromm-finnn <kdc5036@gmail.com>
2022-07-24 22:18:54 +0900

    edit App & Expenseitem components to use dynamic State Data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,13 +4,6 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("update!");
-    console.log("update!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -18,7 +11,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
